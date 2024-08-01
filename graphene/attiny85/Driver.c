e64cc52d1754c55084ad979d221c4f2e5ae2e032
Mauricio <maurio.aravena@sansano.usm.cl>
2024-08-01 02:27:51 -0400

    Not working

--- graphene/attiny85/Driver.c
+++ graphene/attiny85/Driver.c
@@ -1,85 +1,111 @@
 #include <avr/io.h>
+#include <avr/interrupt.h>
 
-#define BUTTON_INPUT PINB2
-#define LED_OUTPUT PB0
+#define LED_STATUS PB4
+#define MCU_ADDRESS 0x08
 
-#define PRESS_BUTTON_MASK 0x80000000
-#define HOLD_BUTTON_MASK 0xFFFFFFFF
+volatile char led = 0;
+volatile char state = 0x00;
+volatile char cmd = 0x00;
+// 0x00 -> Listening address
+// 0x01 -> Writing preparation
+// 0x02 -> Read preparation
+// 0x03 -> Writing process
 
-// Globals
-uint8_t out_state = 0;
-
-// -> Button debouncer
-uint32_t button_debounce;
-uint8_t button_state;
-
-uint8_t readButton(uint32_t *state)
+int main()
 {
-    /// The button has negative logic, a press corresponds to a low level
-    uint8_t btn_read = (~PINB & _BV(BUTTON_INPUT)) >> BUTTON_INPUT;
-    /// The read value in inverted for positive logic and shifted
-    /// Read and shift for debouncing
-    *state = (*state << 1) | btn_read;
-    switch (*state)
-    {
-    case PRESS_BUTTON_MASK:
-        return 0x01;
-    case HOLD_BUTTON_MASK:
-        return 0x02;
-    default:
-        return 0x00;
-    }
-}
 
-void enableTWI()
-{
-    USICR = 0;
-    USICR |= _BV(USICS1); // External clock
-    USICR |= _BV(USIWM1); // TWI mode
-}
+    // Set outputs
+    DDRB |= _BV(LED_STATUS);
 
-void disableTWI()
-{
-    USICR &= ~_BV(USIWM1); // DISABLE TWI
-}
+    // Set the output to LOW to start
+    // PORTB &= ~_BV(LED_STATUS);
+    PORTB |= _BV(LED_STATUS);
 
-int main(void)
-{
-    // Initialize button state
-    button_debounce = 0U;
-    button_state = 0x00; // IDLE
+    // Initialize i2c
+    // -> SET SDA AND SCL as inputs
+    DDRB &= ~_BV(PB0); // SDA
+    DDRB &= ~_BV(PB2); // SCL
+    // -> SET TWI interface register
+    USICR = 0;            // Clear the register
+    USICR |= _BV(USISIE); // Start condition interrupt enable
+    // USICR |= _BV(USIOIE); // Counter overflow interrupt enable -> The counter is used to count the number of bits TX/RX
+    USICR |= _BV(USIWM1) | _BV(USIWM0); // ENABLE TWI
+    USICR |= _BV(USICS1);               // External clock
 
-    // SETUP IO
-    DDRB = 0;
-    DDRB &= ~_BV(BUTTON_INPUT);
-    DDRB |= _BV(LED_OUTPUT);
-
-    // CLEAR OUTPUT VALUES
-    PORTB = 0;
-    PORTB |= _BV(PB2); // ENABLE PULLUP
-
+    // Enable Interrupts
+    sei();
 
     while (1)
     {
-        // Read button
-        button_state = readButton(&button_debounce);
-
-        if (button_state == 0x01)
+        if (led)
         {
-            out_state = 0x01; // Enable TWI interface
+            PORTB |= _BV(LED_STATUS);
         }
-
-        // Check TWI status
-        if(out_state == 0x01)
+        else
         {
-            enableTWI();
-            out_state = 0x02; // Next state
+            PORTB &= ~_BV(LED_STATUS);
         }
+    }
+}
 
-        // Start listening
-        if(out_state == 0x02)
+ISR(USI_START_vect)
+{
+    USICR |= _BV(USIOIE); // Enable counter overflow interrupt
+    USISR |= _BV(USISIF); // Clear the start interrupt flag and release the SCL line
+    USISR &= 0xF0;        // Reset the counter
+}
+
+ISR(USI_OVF_vect)
+{
+    switch (state)
+    {
+    case 0x00: // Addressing
+    {
+        // Check if being addressed
+        if ((USIDR >> 1) == MCU_ADDRESS)
         {
-
+            // Check if R/W
+            if (USIDR & 0x01)
+                state = 0x02; // Read operation
+            else
+            {
+                state = 0x01; // Write operation
+            }
+            DDRB |= _BV(PB0); // Set as output -> Default value low
+            USISR &= 0xF0;    // clear the counter before assigning value
+            USISR |= 0xFE;    // clear the overflow flag and set counter to 14-one cycle before overflow
+            // USISR |= _BV(USIOIF);
         }
+        else // Reset and wait for new start condition
+        {
+            if (led)
+                led = 0;
+            else
+                led = 1;
+            USICR &= ~_BV(USIOIE); // Disable counter overflow interrupt
+            USISR |= _BV(USIOIF);  // clear the overflow flag and release SCL line
+            state = 0x00;
+        }
+        break;
+    }
+
+    case 0x01: // Write preparations
+    {
+        state = 0x03;
+        DDRB &= ~_BV(PB0);    // Release SDA line
+        USISR |= _BV(USIOIF); // Clear overflow flag and release SCL line
+        break;
+    }
+
+    case 0x03: // Writing process
+    {
+        state = 0x00; // ASSUMGIN ONLY ONE BYTE
+        cmd = USIDR;  // Read the cmd
+        // Prepare for another write
+        USICR &= ~_BV(USIOIE); // Disable counter overflow interrupt
+        USISR |= _BV(USIOIF);  // Clear the overflow flag and release SCL line
+        break;
+    }
     }
 }
